44e4e3e761a62811bad059f90971116448107a0c
AllenIverson25 <526jfriedman@frhsd.com>
2024-11-06 10:43:26 -0500

    finished prompts

---
+++ script.js
@@ -0,0 +1,18 @@
+let name1 = prompt('Enter A Name?')
+let emotion = prompt('Enter an emotion?')
+let adjective = prompt('Enter an adjective?')
+let name2 = prompt('Another Name?')
+let adjective2 = prompt('Another Adjective?')
+let exclamation = prompt('Word shouted from the crowd?')
+let verb = prompt('Enter a verb?')
+let adjective3 = prompt('Enter Anooother Adjective?')
+let noun = prompt('Enter a noun?')
+let time = prompt('Enter a time?')
+let adjective4 = prompt('Enter last adjective dont worry?')
+
+
+let myStory = `Hello ${userName}, How are you doing today? How does it feel to be ${userAge} years old?`
+console.log(myStory)
+
+document.getElementById('story').innerHTML = myStory;
+
