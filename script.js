83b43a68c66679175fa5af5df7d12c5ef1ba3c95
AllenIverson25 <526jfriedman@frhsd.com>
2024-11-07 18:30:19 +0000

    added a button will work later

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function generateStory(){
+    document.getElementById('generateButton').classList.add('hidden');
+
 let name1 = prompt('Enter A Name?')
 let emotion = prompt('Enter an emotion?')
 let adjective = prompt('Enter an adjective?')
@@ -11,8 +14,11 @@ let time = prompt('Enter a time?')
 let adjective4 = prompt('Enter last adjective dont worry?')
 
 
-let myStory = `Hello ${userName}, How are you doing today? How does it feel to be ${userAge} years old?`
+let myStory = `The sun was setting, and the stadium was packed. The mile race was about to begin, and ${name1} was feeling ${emotion}. As the starting gun went off, ${name1} surged ahead, cutting through the ${adjective} air. The first lap felt smooth, but soon the competition was breathing down their neck.
+On the second lap, ${name1} saw ${name2} pushing hard, their strides looking ${adjective2}, but ${name1} wasn’t backing down. They could hear the crowd yelling “${exclamation}!” as they rounded the turn. With a burst of energy, ${name1} ${verb} past a ${adjective3} opponent, determined to take the lead.
+The final lap was intense. Every step felt like they were running through ${noun}, but ${name1} pushed harder, ignoring the burning in their legs. As they crossed the finish line in a blazing ${time}, the crowd went crazy. ${name1} had done it—they were the ${adjective4} champion of the mile!`
 console.log(myStory)
 
 document.getElementById('story').innerHTML = myStory;
 
+}
