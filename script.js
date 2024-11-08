0dae816c7859c6157a53f6684fa55f79785cba48
AllenIverson25 <526jfriedman@frhsd.com>
2024-11-08 19:44:00 +0000

    before changing into cards

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ let adjective4 = prompt('Enter last adjective dont worry?')
 
 
 let myStory = `The sun was setting, and the stadium was packed. The mile race was about to begin, and <b>${name1}</b> was feeling <b>${emotion}</b>. As the starting gun went off, <b>${name1}</b> surged ahead, cutting through the <b>${adjective}</b> air. The first lap felt smooth, but soon the competition was breathing down their neck.
-On the second lap, <b>${name1}</b> saw <b>${name2}</b> pushing hard, their strides looking <b>${adjective2}</b>, but <b>${name1}</b> wasn’t backing down. They could hear the crowd yelling “<b>${exclamation}</b>!” as they rounded the turn. With a burst of energy, <b>${name1}</b> <b>${verb}</b> past a <b>${adjective3}</b> opponent, determined to take the lead.
+
+let myStory1 = On the second lap, <b>${name1}</b> saw <b>${name2}</b> pushing hard, their strides looking <b>${adjective2}</b>, but <b>${name1}</b> wasn’t backing down. They could hear the crowd yelling “<b>${exclamation}</b>!” as they rounded the turn. With a burst of energy, <b>${name1}</b> <b>${verb}</b> past a <b>${adjective3}</b> opponent, determined to take the lead.
+
 The final lap was intense. Every step felt like they were running through <b>${noun}</b>, but <b>${name1}</b> pushed harder, ignoring the burning in their legs. As they crossed the finish line in a blazing <b>${time}</b>, the crowd went crazy. <b>${name1}</b> had done it—they were the <b>${adjective4}</b> champion of the mile!`
 console.log(myStory)
 
